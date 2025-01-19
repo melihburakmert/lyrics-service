bd12fecda06e11d47104cce25e8f0e8e9b9fec6a
melihburakmert <melih.burak.mert@gmail.com>
2025-01-20 00:21:32 +0100

    Initial commit

---
+++ src/main/kotlin/mbm/lyrics_service/LyricsServiceApplication.kt
@@ -0,0 +1,11 @@
+package mbm.lyrics_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LyricsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LyricsServiceApplication>(*args)
+}
